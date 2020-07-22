5bf1f7485fb79aa0d69a8c12c8d249f8574da9b8
Maria Dobria <maria.dobria@1and1.ro>
2020-07-22 14:49:44 +0300

    last updates

--- src/main/java/com/example/democonfig/DemoConfigApplication.java
+++ src/main/java/com/example/democonfig/DemoConfigApplication.java
@@ -2,12 +2,12 @@ package com.example.democonfig;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.config.server.EnableConfigServer;
-//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-//@EnableEurekaClient
-//@EnableConfigServer
+@EnableConfigServer
+@EnableEurekaClient
 public class DemoConfigApplication {
 
 	public static void main(String[] args) {
